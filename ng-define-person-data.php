d4e583a143b977fec92e4b5d09aafea354c127f0
Pm Gohil <86161081+pmgohil45@users.noreply.github.com>
2024-06-18 21:08:12 +0530

    Add files via upload

---
+++ ng-define-person-data.php
@@ -0,0 +1,8 @@
+{
+  "records": [
+    { "Name": "Prakash Gohil", "Country": "India" },
+    { "Name": "John Doe", "Country": "USA" },
+    { "Name": "Anna Smith", "Country": "UK" },
+    { "Name": "Peter Jones", "Country": "Canada" }
+  ]
+}
